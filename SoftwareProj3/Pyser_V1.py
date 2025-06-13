a12dea0181c62141efc3ca7d81d22ff636ebf821
Tim802 <timothy.bakkerhuxley@education.nsw.gov.au>
2025-06-13 19:34:22 +1000

    Organised some code. Added classes and better modulisation to code

--- SoftwareProj3/Pyser_V1.py
+++ SoftwareProj3/Pyser_V1.py
@@ -2,34 +2,53 @@ import serial
 import time
 import customtkinter as tkinter
 
-port = 'COM5'
-baud = 115200
+class SliderFrame(tkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
 
-#arduino = serial.Serial(port, baud, timeout=.1)
-app = tkinter.CTk()
-app.title("Shmingus")
-app.geometry("400x150")
+        #instantiate sliders here
+        self.slider1 = Slider(0,0,0,180,self)
+        self.slider2 = Slider(1,0,0,180,self)
+        self.slider3 = Slider(2,0,0,180,self)
 
-class Slider:
-    def __init__(self, row, col, min, max):
+    def slider_event_print(self):
+        print(
+            f"Cont1: {self.slider1.val}, {self.slider1.byte}\nCont2: {self.slider2.val}, {self.slider2.byte}\nCont3: {self.slider3.val}, {self.slider3.byte}\n"
+        )
+
+class Slider(SliderFrame):
+    def __init__(self, row, col, min, max, parentframe):
         self.row = row
         self.col = col
         self.min = min
         self.max = max
-        self.slider = tkinter.CTkSlider(app, from_=self.min, to=self.max, command=self.slider_event)
+        self.frame = parentframe
+        self.val = 90
+        self.byte = self.val.to_bytes()
+        self.slider = tkinter.CTkSlider(self.frame, from_=self.min, to=self.max, command=self.slider_event)
         self.slider.grid(row=self.row, column=self.col, padx=20, pady=20)
 
     def slider_event(self, val):
         #round val to int
         inpval = round(val)
-        #convert val to byte
-        byteinp = inpval.to_bytes()
         self.val = inpval
+        #convert val to byte
+        byteinp = self.val.to_bytes()
         self.byte = byteinp
+        self.frame.slider_event_print()
 
-#init the slider controls
-Slider1 = Slider(0,0,0,180)
-Slider2 = Slider(1,0,0,180)
-Slider3 = Slider(2,0,0,180)
-#run main loop of app
+class App(tkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Shmingus')
+        self.geometry('400x180')
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.slider_frame = SliderFrame(self)
+        self.slider_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+
+#initialise the app
+app = App()
 app.mainloop()
